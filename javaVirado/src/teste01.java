0746067f8e96d6e2d8ce7f35c080f2934a4970e3
Hitech1111 <gcaio995@gmail.com>
2023-06-24 19:40:28 -0300

    commit classes

---
+++ javaVirado/src/teste01.java
@@ -0,0 +1,26 @@
+import escola.estudante;
+
+
+public class teste01 {
+
+    public static void main(String[] args) {
+        estudante estudante = new estudante();
+        estudante.nome = "alek";
+        estudante.idade = 18;
+        estudante.matricula = 25852;
+        estudante.sexo = 'm';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.matricula);
+        System.out.println(estudante.sexo);
+
+
+
+
+
+    }
+
+
+
+}
